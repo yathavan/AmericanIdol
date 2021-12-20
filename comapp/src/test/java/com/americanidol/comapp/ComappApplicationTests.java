d3b1000096c14b53848c12925fe773e4af456d09
Yathavan Piraba <yathavan7@gmail.com>
2021-12-20 10:54:27 +0530

    Springboot app

---
+++ comapp/src/test/java/com/americanidol/comapp/ComappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.americanidol.comapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
